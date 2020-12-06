edb597801d6c975db4e6670b6f0fbc7c24409db7
Alex <A1exeyP@ukr.net>
2020-12-06 12:45:23 +0200

    modified MyThread class, add 'greeting' and 'help' messages

--- NewLibrary/serverLibrary.py
+++ NewLibrary/serverLibrary.py
@@ -11,16 +11,25 @@ class MyThread(threading.Thread):
         self.addr = addr
 
     def run(self):
-        i = 3
-        while i > 0:
+        while True:
             try:
-                msg.send_msg(self.conn, 'What do u want?')
+                greeting_message = 'Hello, this is library server. What do u want to do? (Type "info" for help)'
+                help_message = 'Server supported next command (without quotes): \n' \
+                               '"all" - shows all books in library \n' \
+                               '"given" - shows given to readers books \n' \
+                               '"available" - shows available books in library \n' \
+                               '"sortT" - shows sorted books by title \n' \
+                               '"sortA" - shows sorted books by author \n' \
+                               '"sortY" shows sorted books by year. \n'
+                msg.send_msg(self.conn, greeting_message)
                 answer = msg.read_msg(self.conn)
                 if answer:
                     if answer == 'exit':
                         msg.send_msg(self.conn, 'Bye')
                         print("Disconnected", self.addr)
                         break
+                    elif answer == 'info':
+                        message = help_message
                     elif answer == 'all':
                         message = library2.national_library.show_all_books()
                     elif answer == 'given':
@@ -35,25 +44,22 @@ class MyThread(threading.Thread):
                         else:
                             message = 'incorrect parameter'
                     else:
-                        message = 'hrrrr'
+                        message = 'unknown command'
                     msg.send_msg(self.conn, message)
             except ValueError:
                 print("Value problems")
-            finally:
-                i -= 1
-                conn.close()
+        conn.close()
 
 
 if __name__ == "__main__":
 
     sock = socket.socket()
     sock.bind(('', 1234))
-    i = 5
-    while i > 0:
-        sock.listen(2)
+    while True:
+        sock.listen(5)
         conn, address = sock.accept()
         my_thread = MyThread(conn, address)
         my_thread.start()
         print("Connected", address)
-        i -= 1
+
 
