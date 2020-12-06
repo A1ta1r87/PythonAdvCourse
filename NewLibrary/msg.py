4e83869bc091ee19818841e15786fbb4c1a08e65
Alex <A1exeyP@ukr.net>
2020-12-06 11:53:14 +0200

    delete try/except block

--- NewLibrary/msg.py
+++ NewLibrary/msg.py
@@ -1,5 +1,5 @@
 def read_msg(connection, size_pack=1024, encoding='utf-8'):
-    try:
+    # try:
         size_msg = int(connection.recv(10).decode())
         msg = ''
         while size_msg > 0:
@@ -7,20 +7,22 @@ def read_msg(connection, size_pack=1024, encoding='utf-8'):
             size_msg -= size_pack
             msg += data
         return msg
-    except Exception:
-        print("some problems with read message")
-        return False
+    # except Exception:
+    #     print("some problems with read message")
+    #     return False
 
 
 def send_msg(connection, msg):
-    try:
+
+    # try:
+        print(connection)
         size_msg = f'{len(msg):10}'
         connection.send(size_msg.encode())
         connection.send(msg.encode())
-        print("Your message was sent successfully!")
+        print("message was sent successfully")
         return True
-    except Exception:
-        print("some problems")
-        return False
+    # except Exception:
+    #     print("some problems")
+    #     return False
 
 
