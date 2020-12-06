2054b1364d0efda5c128875d3192fdf4f309f53f
Alex <A1exeyP@ukr.net>
2020-12-06 12:42:21 +0200

    add "while True" loop

--- NewLibrary/clientBase.py
+++ NewLibrary/clientBase.py
@@ -9,27 +9,31 @@ def is_json(string):
     except ValueError:
         return False
     return True
-i = 5
-while i > 0:
-    with socket.socket() as client_sock:
-        address = ('localhost', 1234)
-        client_sock.connect(address)
-        try:
+
+
+with socket.socket() as client_sock:
+    address = ('localhost', 1234)
+    client_sock.connect(address)
+    try:
+        while True:
             query = msg.read_msg(client_sock)
             if query:
                 print(query)
                 answer = input()
                 client_answer = msg.send_msg(client_sock, answer)
                 server_answer = msg.read_msg(client_sock)
+                if server_answer == 'Bye':
+                    print(server_answer)
+                    break
                 if is_json(server_answer):
                     server_answer = json.loads(server_answer)
                     for index, value in server_answer.items():
                         print(f'{index}  {value}')
                 else:
                     print(server_answer)
-        except TypeError:
-            print("Connection failed")
-    i -= 1
+    except TypeError:
+        print("Connection failed")
+
 
 
 
